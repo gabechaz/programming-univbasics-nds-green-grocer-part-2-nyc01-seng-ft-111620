8029e11b69c836644d537b2078f1ea1dfe0ee644
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-15 21:11:37 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,22 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ # create a consolidated cart
+consolidated_cart = consolidate_cart(item_array)
+# check if there is a coupon for an item in the cart
+cart.each do |item_hash|
+  test_att = :item
+  coupons.each do |coupon_array|
+    if :item == test_att && 
+      
+    end
+end
+end
+  
+# check if the amount of items in the cart qualifies for a coupon. 
+# if so, remove that many items from the cart and add the coupon hash to the array
+#   use math to calculate the per unit price of the discounted produce
+#   return the altered array of hashes
 end
 
 def apply_clearance(cart)
